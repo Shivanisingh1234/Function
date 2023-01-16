ff3e18157f212838b058e1ea23d961c7b2f08a21
Shivanisingh1234 <122779359+Shivanisingh1234@users.noreply.github.com>
2023-01-16 14:22:05 +0530

    Add files via upload

---
+++ fun.java
@@ -0,0 +1,10 @@
+public class fun {
+    public static void main(String[] args) {
+       int ans = function(4, 6);
+       System.out.println(ans);
+    }
+    public static int function( int a , int b) {
+        return a+b;
+        
+    }
+}
